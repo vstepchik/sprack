4e405b4dcbaa51a6e750416cd37112b642308e36
vitalatron <vitalatron@gmail.com>
2017-10-15 11:13:33 +0300

    removed redundant image cloning

--- src/sprack/drawing.rs
+++ src/sprack/drawing.rs
@@ -1,19 +1,19 @@
 extern crate image;
 
-use image::*;
+use image::RgbaImage;
+use image::imageops::{rotate270, replace as draw_img};
 use std::path::Path;
 use super::algorithm::Bin;
 
 pub fn draw_bin(path: &AsRef<Path>, images: &[RgbaImage], bin: &Bin) -> u64 {
   let mut atlas = RgbaImage::new(bin.size.w, bin.size.h);
   for p in &bin.placements {
-    let img: ImageBuffer<_, _> = if p.rect.flipped {
-      imageops::rotate270(&images[p.index as usize])
+    if p.rect.flipped {
+      draw_img(&mut atlas, &rotate270(&images[p.index as usize]), p.rect.x, p.rect.y);
     } else {
-      images[p.index as usize].to_owned()
+      draw_img(&mut atlas, &images[p.index as usize], p.rect.x, p.rect.y);
     };
-    image::imageops::replace(&mut atlas, &img, p.rect.x, p.rect.y);
   }
-  atlas.save(path).unwrap();
+  atlas.save(path).expect(format!("Failed to save atlas {:?}", path.as_ref()).as_ref());
   path.as_ref().metadata().unwrap().len()
 }
