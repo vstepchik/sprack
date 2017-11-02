0948670f15a8d444f8ee085384421641b1d18170
Vitaliy Stepchik <vitalatron@gmail.com>
2017-11-02 17:48:53 +0200

    command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#[macro_use]
+extern crate serde_derive;
 extern crate image;
 extern crate rayon;
+extern crate docopt;
 extern crate sprack;
 
 mod sprack_bin;
@@ -9,23 +12,91 @@ use sprack::*;
 use std::path::Path;
 use rayon::prelude::*;
 use image::RgbaImage;
+use docopt::Docopt;
 
 const PNG_EXT: &str = "png";
 
+const USAGE: &'static str = "
+Usage:  sprack [options] ([-w SIDE] [-h SIDE] | [-s SIDE]) [--demo | --help | <files>...]
+
+Options:
+    -r, --recursive             Recursively descend into directories.
+    -o, --out=DIR               Output dir for results, overwrites existing files [default: ./out].
+    -w, --width=SIDE            Atlas width [default: 1024].
+    -h, --height=SIDE           Atlas height [default: 1024].
+    -s, --size=SIDE             Atlas width and height.
+    -f, --flipping              Allow placement of sprites rotated by 90 degrees.
+    -t, --trim                  Trim resulting images to minimal size.
+    -i, --increments-count=NUM  Allows incremental atlas size growth. 0 means atlas starts at
+                                specified size, without increments. If NUM is > 0 starts at
+                                INC size and if sprite doesn't fit - grows by another INC, where
+                                INC = min(1, SIDE/(NUM+1)). Allowed values are 0..255, the higher
+                                the value - the more time packing will take [default: 0].
+    -k, --keep-work-dir         Do not delete temporary files after work.
+        --demo                  Generate in-memory random colored rectangles with up arrows and
+                                compact them into sprite atlases.
+    -h, --help                  Show this help message.
+";
+
+#[derive(Deserialize, Debug)]
+struct Args {
+  arg_files: Vec<String>,
+  flag_out: String,
+  flag_increments_count: u8,
+  flag_width: u32,
+  flag_height: u32,
+  flag_size: Option<u32>,
+  flag_trim: bool,
+  flag_flipping: bool,
+  flag_demo: bool,
+  flag_keep_work_dir: bool,
+  flag_recursive: bool,
+  flag_help: bool,
+}
+
+impl<'a> From<&'a Args> for RunOptions<'a> {
+  fn from(args: &'a Args) -> Self {
+    let bin_size = if let Some(size) = args.flag_size {
+      Dimension { w: size, h: size }
+    } else {
+      Dimension { w: args.flag_width, h: args.flag_height }
+    };
+
+    let pack_options = PackOptions {
+      bin_size,
+      flipping: args.flag_flipping,
+      trim: args.flag_trim,
+      atlas_compact_steps: args.flag_increments_count,
+      ..Default::default()
+    };
+
+    RunOptions {
+      keep_work_dir: args.flag_keep_work_dir,
+      input_paths: args.arg_files.iter().map(|f| Path::new(f.as_str())).collect(),
+      output_path: Path::new(args.flag_out.as_str()),
+      demo_run: args.flag_demo,
+      recursive: args.flag_recursive,
+      pack_options,
+      ..Default::default()
+    }
+  }
+}
+
 fn main() {
+  let args: Args = Docopt::new(USAGE)
+    .and_then(|d| d.deserialize())
+    .unwrap_or_else(|e| e.exit());
+
+  println!("Args: {:?}", args);
   let work_dir = new_work_dir().expect("Failed to create work dir");
   println!("Work dir is {:?}", &work_dir);
+
+  let options = RunOptions::from(&args);
+  for path in &options.input_paths {
+    println!("> {:?}", path)
+  }
+
   let samples = generate_rectangles(200);
-  let options = RunOptions {
-    keep_work_dir: true,
-    pack_options: PackOptions {
-      flipping: true,
-      atlas_compact_steps: 3,
-      bin_size: Dimension { w: 512, h: 512 },
-      ..Default::default()
-    },
-    ..Default::default()
-  };
   draw_samples(&work_dir, &samples);
 
   let input = samples.iter().map(|s| Dimension { w: s.width(), h: s.height() }).collect::<Vec<_>>();
--- src/sprack_bin/tool.rs
+++ src/sprack_bin/tool.rs
@@ -5,16 +5,22 @@ use std::path::Path;
 
 pub struct RunOptions<'a> {
   pub pack_options: PackOptions<'a>,
+  pub input_paths: Vec<&'a Path>,
   pub output_path: &'a Path,
   pub keep_work_dir: bool,
+  pub demo_run: bool,
+  pub recursive: bool,
 }
 
 impl<'a> Default for RunOptions<'a> {
   fn default() -> RunOptions<'a> {
     RunOptions {
       pack_options: PackOptions { ..Default::default() },
+      input_paths: vec![],
       output_path: Path::new("out"),
       keep_work_dir: false,
+      demo_run: false,
+      recursive: false,
     }
   }
 }
